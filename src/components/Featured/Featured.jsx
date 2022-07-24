5455fb638a1aad6fd2925073530e4005e831dce0
miltonblack <arziblack2@gmail.com>
2022-07-24 06:28:23 -0700

    created featured component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Featured.css'
-
+import { FaPlay, FaInfo } from 'react-icons/fa'
 const Featured = () => {
   return (
     <div className='featured'>
         <img src='./img.jpeg' alt=''/>
-        <div className='info'></div>
+        <div className='info'>
+            <img src='./img.jpeg' alt=''/>
+            <span className='desc'> 
+                this is an action movie, sit back, watch and relax, catch thrills, suspense and drama.
+            </span>
+            <div className='buttons'>
+                <button className='play'>
+                    <FaPlay/>
+                    <span>play</span>
+                </button>
+                <button className='more'>
+                    <FaInfo/>
+                    <span>more info</span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
