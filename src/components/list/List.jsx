9eb1e24def5b252cc0dbda92bdd57f3b7759783f
miltonblack <arziblack2@gmail.com>
2022-07-25 15:52:45 -0700

    Styled List.css, ListItem.css, Created a handleClick function and Used the useRef hook in place of query selector

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './List.css'
 import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md'
 import ListItem from '../ListItem/ListItem'
 
 function List() {
+/*--instead of query selector we use useRef Hook--*/
+  const listRef = useRef()
+
+
+  const handleClick = (direction) => {
+    let space = listRef.current.getBoundingClientRect().x - 50
+    if (direction === "left") {
+      listRef.current.style.transform = `translateX(${230 + space}px)`
+    }
+    if (direction === "right") {
+      listRef.current.style.transform = `translateX(${-230 + space}px)`
+    }
+    // console.log(space)
+  }
   return (
     <div className='list'>
         <span className='listtitle'>Continue to Watch</span>
         <div className='wrapper'>
-          <MdArrowBackIos/>
-          <div className='container'>
+          <MdArrowBackIos className='sliderarrow left' onClick={() => handleClick("left")}/>
+          <div className='container' ref={listRef}>
+            <ListItem/>
+            <ListItem/>
+            <ListItem/>
             <ListItem/>
             <ListItem/>
             <ListItem/>
@@ -24,7 +41,7 @@ function List() {
             <ListItem/>
             <ListItem/>
           </div>
-          <MdArrowForwardIos/>
+          <MdArrowForwardIos className='sliderarrow right' onClick={() => handleClick("right")}/>
         </div>
     </div>
   )
